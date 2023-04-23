6f3c341ddc8dfe08dc551b2359f1485060658286
Paulo Santos <102436341+pauloninja@users.noreply.github.com>
2023-04-23 14:27:41 -0300

    Atualizar

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// Seleção de elementos
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const editForm = document.querySelector("#edit-form");
+const editInput = document.querySelector("#todo-form");
+const cancelEditBtn = document.querySelector("#cancel-edit-btn");
+
+
+// Funções
+
+
+
+// Eventos
